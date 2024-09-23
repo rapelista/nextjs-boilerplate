bb2b0b5bcd6355bbdff54cd2295aad6164f50f8c
Farih Akmal Haqiqi <akmal@gamatecha.com>
2024-09-23 13:16:38 +0700

    chore: rename example folder

--- src/app/(panel)/dashboard/page.tsx
+++ src/app/(panel)/dashboard/page.tsx
@@ -1,5 +1,5 @@
-import { actions } from '~/components/(entity)/unit/actions';
-import { columns } from '~/components/(entity)/unit/columns';
+import { actions } from '~/components/(example)/unit/actions';
+import { columns } from '~/components/(example)/unit/columns';
 import { DataTableMaster } from '~/components/data-table-master';
 
 export default function Page() {
